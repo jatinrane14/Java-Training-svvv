2528c076a4d2ecd63b3d126ca3bf7507ff9a2802
jatinrane14 <jatinrane23@gmail.com>
2026-04-24 15:06:00 +0530

    Add ElectricityBill excercise

---
+++ Practice/ElectricityBill.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ElectricityBill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the total unit used: ");
+        double unit = sc.nextDouble();
+        double bill;
+        if(unit <= 100){
+            bill  = unit * 1.5;
+        }else if(unit <=200){
+            bill = (100 * 1.5) + ((unit - 100) * 3.5);
+        }else{
+            bill = (100 * 1.5) + 100* 3.5 + (unit - 200)*5;
+        }
+        bill+=50;
+        if(bill>2000){
+            double tenPerc = bill * 0.10;
+            bill+= tenPerc;
+        }
+
+        System.out.println("Total Electricity bill is : " + bill);
+    }
+}
