13f04c50c0d001f2767d5f9fda6932b8dd3fbf8a
jatinrane14 <jatinrane23@gmail.com>
2026-04-24 16:28:59 +0530

    Login Attempt

---
+++ Practice/LoginAttempt.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LoginAttempt {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        String demoUsername = "jatinrane";
+        String demopass = "123456789";
+
+        System.out.println("======Login Yout Account========");
+        boolean isLoggedin = false;
+        int loginAttemplt =0;
+        System.out.println("Enter username: ");
+        String username = sc.nextLine();
+        do {
+            System.out.println("Enter password: ");
+            String password = sc.nextLine();
+            if(username.equals(demoUsername)  && password.equals(demopass)){
+                System.out.println("Login successfull!");
+                isLoggedin = true;
+                break;
+            }else{
+                System.out.println("Username or password is incorrect!");
+                loginAttemplt++;
+            }
+        } while (!isLoggedin && loginAttemplt<3);
+        if (loginAttemplt >=3) {
+            System.out.println("Account Locked!");
+        }
+    }
+}
